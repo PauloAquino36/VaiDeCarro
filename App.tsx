7c3748ee82206154ac3ce5cf510a59c9efd957fd
Paulo Aquino <paulo.aquino@estudante.ufjf.br>
2025-02-12 13:50:11 -0300

    Adicione o componente Login e atualize o layout do aplicativo

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './Telas/Login';
 import BotaoContornado from './Componentes/BotaoContornado';
 
 export default function App() {
 
-  const handlePress = () => {
-    alert('Botão pressionado!');
-  };
-
   return (
     <View style={styles.container}>
-      <BotaoContornado title="Pressione-me" onPress={handlePress} />
       <StatusBar style="auto" />
+      <Login />
     </View>
   );
 }
@@ -19,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#191919',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Telas/Login.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { View, TextInput, Image, StyleSheet, Dimensions } from 'react-native';
+import BotaoContornado from '../Componentes/BotaoContornado';
+
+const { width } = Dimensions.get('window');
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/Imgs/VaiDeCarro_logo.png')} style={styles.logo} />
+
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.input} 
+          keyboardType="email-address" 
+          autoCapitalize="none" 
+          placeholder="E-mail"
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.input}  
+          secureTextEntry 
+          placeholder="Senha"
+        />
+      </View>
+
+      <BotaoContornado title="Entrar" onPress={() => {}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: width * 0.1, // Padding responsivo
+    backgroundColor: '#191919',
+  },
+  logo: {
+    width: width * 0.4, // 40% da largura da tela
+    height: width * 0.4, // Mantendo proporção
+    marginBottom: width * 0.08,
+    resizeMode: 'contain',
+  },
+  inputContainer: {
+    width: width * 0.8, // 80% da largura da tela
+    marginBottom: width * 0.04,
+  },
+  input: {
+    width: '100%',
+    paddingVertical: width * 0.05, // Tamanho do padding ajustável
+    paddingHorizontal: width * 0.05,
+    borderWidth: 2,
+    backgroundColor: '#FEFFF5',
+    borderColor: '#38B6FF',
+    borderRadius: 27,
+    fontSize: width * 0.04, // Ajustando tamanho da fonte
+  },
+});
+
+export default Login;
